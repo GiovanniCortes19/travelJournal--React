ad4975ad68f098f84e66b3124ed7ac589cfe081f
GiovanniCortes <giocortes19@gmail.com>
2023-03-31 13:47:06 -0400

    create Card component template that access necesary data from props where needed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,25 @@
 export default function Card(props) {
     return (
         <div className="card">
-            <img src='' className="card-img" />
-            <div>
-                <h1>test card</h1>
+
+            <img src={props.item.imageURL} className="card-img" />
+
+            <div className="card-text">
+                <div className="location-info">
+                    <img src='' className="pin-icon" />
+                    <span>{props.item.location}</span>
+                    <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+                </div>
+
+                <h1 className="title">{props.item.title}</h1>
+
+                <div className="text-block">
+                    <p className="date">{props.item.startDate} - {props.item.endDate}</p>
+                    <p className="description">{props.item.description}</p>
+                </div>
+
             </div>
+
         </div>
     )
 }
