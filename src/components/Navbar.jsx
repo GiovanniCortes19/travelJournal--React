4b2e0879ba6e9ce3db52ccb5b7f0596e89321af8
GiovanniCortes <giocortes19@gmail.com>
2023-03-31 16:24:04 -0400

    added world-icon image and google Poppins font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import worldIcon from './world-icon.png'
+
 export default function Navbar() {
     return (
         <nav className="navbar">
+            <img src={worldIcon} alt='world-icon-img' className='world-icon' />
             <h1>my travel journal</h1>
         </nav>
     )
