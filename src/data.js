7205eb3d8c803b872446bbf28e86ae0fd5f334bf
GiovanniCortes <giocortes19@gmail.com>
2023-03-31 19:47:09 -0400

    mobile responsive dimensions

--- src/data.js
+++ src/data.js
@@ -32,4 +32,26 @@ export default [
           "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
       },
+      {
+        key: 4,
+        title: "Paris",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/ghXB4fByH12gzABy9",
+        startDate: "07 Apr, 2022",
+        endDate: "13 Apr, 2022",
+        description:
+          "Paris is located in northern central France, in the region of Île-de-France. It is situated on the banks of the Seine River.",
+        imageUrl: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2673&q=80",
+      },
+      {
+        key: 5,
+        title: "Le Mont-Saint-Michel",
+        location: "France",
+        googleMapsUrl: "https://goo.gl/maps/EZCCN5NZ61oDTDrA7",
+        startDate: "14 Apr, 2022",
+        endDate: "16 Apr, 2022",
+        description:
+          "Le Mont-Saint-Michel is located in Normandy, France, on the country's northwestern coast. Specifically, it is situated on a small rocky island at the mouth of the Couesnon River.",
+        imageUrl: "https://images.unsplash.com/photo-1596436831831-87dd84a69101?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+      },
     ]
